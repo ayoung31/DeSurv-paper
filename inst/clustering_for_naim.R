64aae511641f1e5b72df0ebc53b0b6fefadb45e1
ayoung31 <ayoung31@ad.unc.edu>
2025-09-24 12:18:34 -0400

    add additional dataset options

--- inst/clustering_for_naim.R
+++ inst/clustering_for_naim.R
@@ -1,13 +1,27 @@
 purrr::walk(list.files("R", full.names = TRUE, pattern = "[.]R$"), source)
 
-### load data
-VAL_DATASETS = c("Dijk","Linehan","Moffitt_GEO_array",
-                 "PACA_AU_array","PACA_AU_seq","Puleo_array")
-VAL_PREFIX = paste0(VAL_DATASETS, collapse = ".")
-data = readRDS(paste0("data/derv/",VAL_PREFIX,"_formatted.rds"))
+### load data (PICK ONE to uncomment below)
 
-data = readRDS(paste0("data/derv/","dijk","_formatted.rds"))
+## combined datasets
+# VAL_DATASETS = c("Dijk","Linehan","Moffitt_GEO_array",
+#                  "PACA_AU_array","PACA_AU_seq","Puleo_array")
+# VAL_PREFIX = paste0(VAL_DATASETS, collapse = ".")
+# data = readRDS(paste0("data/derv/",VAL_PREFIX,"_formatted.rds"))
 
+## dijk
+# data = readRDS(paste0("data/derv/","dijk","_formatted.rds"))
+
+## moffitt
+# data = readRDS(paste0("data/derv/","moff","_formatted.rds"))
+
+## paca seq
+# data = readRDS(paste0("data/derv/","paca_seq","_formatted.rds"))
+
+## paca array
+# data = readRDS(paste0("data/derv/","paca_array","_formatted.rds"))
+
+## puleo
+# data = readRDS(paste0("data/derv/","puleo","_formatted.rds"))
 
 ### load top genes
 load("data/top_genes/selected_model.RData")
