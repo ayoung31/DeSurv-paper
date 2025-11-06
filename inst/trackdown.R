0ee57a8e096ac36e92aa9ea3f9caabb28592fdc8
ayoung31 <ayoung31@ad.unc.edu>
2025-11-05 19:27:22 -0500

    final paper draft

--- inst/trackdown.R
+++ inst/trackdown.R
@@ -14,7 +14,7 @@ trackdown::update_file(
   gfile = "paper",   # optional: the name it will have in Google Drive
   gpath = "desurv_paper_trackdown",    # folder in Drive (will create if missing)
   hide_code = FALSE,      # hide code + header from collaborators?
-  path_output = "paper/paper.pdf"     # also upload the rendered PDF or HTML alongside
+  path_output = "paper/paper_ntop=50_ngene=2000.pdf"     # also upload the rendered PDF or HTML alongside
 )
 
 trackdown::update_file(
@@ -24,7 +24,7 @@ trackdown::update_file(
   hide_code = FALSE      # hide code + header from collaborators?
 )
 
-trackdown::upload_file(
+trackdown::update_file(
   "paper/03_methods.Rmd",           # path to your .Rmd / .qmd / .Rnw
   gfile = "methods",   # optional: the name it will have in Google Drive
   gpath = "desurv_paper_trackdown",    # folder in Drive (will create if missing)
@@ -32,13 +32,13 @@ trackdown::upload_file(
 )
 
 trackdown::upload_file(
-  "paper/02_introduction.Rmd",           # path to your .Rmd / .qmd / .Rnw
+  "paper/02_introduction_30102025.Rmd",           # path to your .Rmd / .qmd / .Rnw
   gfile = "introduction",   # optional: the name it will have in Google Drive
   gpath = "desurv_paper_trackdown",    # folder in Drive (will create if missing)
   hide_code = FALSE      # hide code + header from collaborators?
 )
 
-trackdown::upload_file(
+trackdown::update_file(
   "paper/05_discussion.Rmd",           # path to your .Rmd / .qmd / .Rnw
   gfile = "discussion",   # optional: the name it will have in Google Drive
   gpath = "desurv_paper_trackdown",    # folder in Drive (will create if missing)
